444988ac3216d4864939bcd463682784f67eeac8
MarinaBraga870 <78864942+MarinaBraga870@users.noreply.github.com>
2021-03-31 19:14:33 -0300

    Digitação 2° parte

--- AC1.ino
+++ AC1.ino
@@ -7,3 +7,18 @@ const int azul = 7;
 bool estadoLedVermelho = false;
 
 //variaveis dos botoes
+const int botao1 = 2;
+const int botao2 = 8;
+unsigned long lastDebounceTime1 = 0;
+const int botaoDelay = 100;
+
+//usado para começar o programa e serve para configurar os pinos para comunicação do serial
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  
+  pinMode(vermelho, OUTPUT);
+  pinMode(verde, OUTPUT);
+  pinMode(azul, OUTPUT);
+  
